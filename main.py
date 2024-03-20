906e888f0307107aa3bcf2994b2af1f5b8edaeb1
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-03-20 10:17:05 +0000

    PP-API-5: populate power plants table, create model and post endpoint

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import Dict
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-from pandas import DataFrame
 import uvicorn
 
-from services.load_data_service import read_from_csv
+from models.power_plants_data import PowerPlantsDataInput
+from services.load_data_service import set_power_plants_data
 
 app = FastAPI(title="Power Plants API", version="0.0.1")
 
@@ -22,9 +22,9 @@ def read_root() -> Dict[str, str]:
     return {"health": "OK"}
 
 
-@app.post("/load-data", response_model=None)
-def handle_data_load() -> DataFrame:
-    read_from_csv()
+@app.post("/load-data", response_model=PowerPlantsDataInput)
+def handle_data_load() -> Response:
+    return set_power_plants_data()
 
 
 if __name__ == "__main__":
--- services/load_data_service.py
+++ services/load_data_service.py
@@ -1,11 +1,16 @@
+from fastapi import Response
+from fastapi.responses import HTMLResponse
 import pandas as pd
-from pandas import DataFrame
+from sqlalchemy import create_engine
+from sqlmodel import SQLModel, Session, select
+
+from config import DATABASE_PASSWORD, DATABASE_USER, DATABASE_HOST
+from models.power_plants_data import PowerPlantsData
+
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}"
 
 
-power_plants_df: DataFrame
-
-
-def read_from_csv() -> DataFrame:
+def read_from_csv() -> pd.DataFrame:
     power_plants_df = pd.read_csv(
         "datasets/global_power_plants.csv",
         usecols=[
@@ -17,11 +22,38 @@ def read_from_csv() -> DataFrame:
             "latitude",
             "longitude",
             "primary_fuel",
-            "other_fuel1",
-            "other_fuel2",
-            "commissioning_year",
-            "owner",
-            "wepp_id",
         ],
+        na_values=["NaN", ""],
     )
     return power_plants_df
+
+
+def set_power_plants_data() -> Response:
+    engine = create_engine(DATABASE_URL, echo=True)
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        existing_records = session.exec(select(PowerPlantsData)).first()
+        if not existing_records:
+            csv_data = read_from_csv()
+            for _, row in csv_data.iterrows():
+                plant = PowerPlantsData(
+                    country=row["country"],
+                    country_long=row["country_long"],
+                    name=row["name"],
+                    gppd_idnr=row["gppd_idnr"],
+                    capacity_mw=row["capacity_mw"],
+                    latitude=row["latitude"],
+                    longitude=row["longitude"],
+                    primary_fuel=row["primary_fuel"],
+                )
+                session.add(plant)
+            session.commit()
+            return HTMLResponse(
+                content="Data added successfully",
+                status_code=200,
+            )
+        else:
+            return HTMLResponse(
+                content="OK, Data already exists",
+                status_code=200,
+            )
